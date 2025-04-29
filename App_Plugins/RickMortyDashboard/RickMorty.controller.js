ec27e63859fccd5cc4f732de983386d2cd00be71
taslemul <taslemul@gmail.com>
2025-04-29 09:49:09 +0100

    Dashboard edit

--- App_Plugins/RickMortyDashboard/RickMorty.controller.js
+++ App_Plugins/RickMortyDashboard/RickMorty.controller.js
@@ -1,13 +1,23 @@
-﻿angular.module("umbraco").controller("RickMortyDashboardController", function ($http) {
+﻿angular.module("umbraco").controller("RickMortyDashboardController", function ($scope, $http, $window) {
   var vm = this;
+
+  vm.message = "";
+  vm.showReload = false;
 
   vm.importCharacters = function () {
     vm.message = "Importing...";
+    vm.showReload = false;
 
     $http.post("/umbraco/api/rickmorty/import").then(function (res) {
       vm.message = res.data;
+      vm.showReload = true;
     }, function (err) {
-      vm.message = "Something went wrong.";
+      console.error("API error:", err);
+      vm.message = err.data || "Something went wrong.";
     });
+  };
+
+  vm.reloadPage = function () {
+    $window.location.reload();
   };
 });
