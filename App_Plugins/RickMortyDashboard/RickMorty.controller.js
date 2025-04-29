52105a5477aea6e0f4ebd9b53b763191e5052682
taslemul <taslemul@gmail.com>
2025-04-29 04:30:25 +0100

    Added functional dashboard that provides feedback

---
+++ App_Plugins/RickMortyDashboard/RickMorty.controller.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco").controller("RickMortyDashboardController", function ($http) {
+  var vm = this;
+
+  vm.importCharacters = function () {
+    vm.message = "Importing...";
+
+    $http.post("/umbraco/api/rickmorty/import").then(function (res) {
+      vm.message = res.data;
+    }, function (err) {
+      vm.message = "Something went wrong.";
+    });
+  };
+});
